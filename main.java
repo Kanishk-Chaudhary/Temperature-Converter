ebce5779a005d98b0131b1fbea74a3b4b29368be
Kanishk Chaudhary <opkanix@gmail.com>
2023-12-24 19:48:37 +0530

    first

--- main.java
+++ main.java
@@ -5,16 +5,16 @@ public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Choose the degree:");
-        System.out.println("1. celcius to ferenheit");
-        System.out.println("2. ferenheit to celcius");
+        System.out.println("1. celcius to fahrenheit");
+        System.out.println("2. fahrenheit to celcius");
         Long a = input.nextLong();
         if (a == 1) {
             System.out.print("Enter the tempreture in celcius : ");
             float c = input.nextFloat();
             float d = (c * 9 / 5) + 32;
-            System.out.println("The given tempreture is in farenheit is : " + d);
+            System.out.println("The given tempreture is in fahrenheit is : " + d);
         } else if (a == 2) {
-            System.out.print("Enter the tempreture in farenheit: ");
+            System.out.print("Enter the tempreture in fahrenheit: ");
             float e = input.nextFloat();
             float f = (e - 32) * 5 / 9;
             System.out.println("The given tempreture is in celcius is: " + f);
