94ddc404a7c135513bdb5beb222db1074232048a
Kanishk Chaudhary <opkanix@gmail.com>
2023-12-25 01:39:15 +0530

    first

--- main.java
+++ main.java
@@ -13,32 +13,32 @@ public class main {
         System.out.println("6. kelvin to fahrenheit");
         Long a = input.nextLong();
         if (a == 1) {
-            System.out.print("Enter the temperature in celcius : ");
+            System.out.print("Enter the temperature in celcius: ");
             float b = input.nextFloat();
             float c = (b * 9 / 5) + 32;
-            System.out.println("The given temperature is in fahrenheit is : " + c);
+            System.out.println("The given temperature is in fahrenheit is: " + c);
         } else if (a == 2) {
             System.out.print("Enter the temperature in fahrenheit: ");
             float b = input.nextFloat();
             float c = (b - 32) * 5 / 9;
             System.out.println("The given temperature is in celcius is: " + c);
         } else if (a == 3) {
-            System.out.print("Enter the temperature in kelvin");
+            System.out.print("Enter the temperature in kelvin: ");
             double b = input.nextFloat();
             double c = (b - 273.15);
             System.out.println("The given temperature in celcius is: " + c);
         } else if (a == 4) {
-            System.out.print("Enter the temperature in celcius");
+            System.out.print("Enter the temperature in celcius: ");
             double b = input.nextFloat();
             double c = (b + 273.15);
             System.out.println("The given temperature in kelvin is: " + c);
         } else if (a == 5) {
-            System.out.print("Enter the temperature in fahrenheit");
+            System.out.print("Enter the temperature in fahrenheit: ");
             double b = input.nextFloat();
             double c = (b - 32) * 5 / 9 + 273.15;
             System.out.println("The given temperature in kelvin is: " + c);
         } else if (a == 6) {
-            System.out.print("Enter the temperature in kelvin");
+            System.out.print("Enter the temperature in kelvin: ");
             double b = input.nextFloat();
             double c = (b - 273.15) * 9 / 5 + 32;
             System.out.println("The given temperature in fahrenheit is: " + c);
